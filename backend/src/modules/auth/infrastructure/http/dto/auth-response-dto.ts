d3f3573f6ab03f07fae1ab0e8f14d42e27c9c909
JesusGuti <jesusgonzales0968@gmail.com>
2026-09-05 23:01:27 -0400

    refactor: add refresh token in sign in response

--- backend/src/modules/auth/infrastructure/http/dto/auth-response-dto.ts
+++ backend/src/modules/auth/infrastructure/http/dto/auth-response-dto.ts
@@ -1,10 +1,22 @@
 import { SignInResult } from '../../../application/use-cases/sign-in.use-case';
+import { RefreshResult } from '../../../application/use-cases/refresh.use-case';
 
 export class SignInResponseDto {
   accessToken!: string;
+  refreshToken!: string;
 
   static fromResult(result: SignInResult): SignInResponseDto {
     const dto = new SignInResponseDto();
+    Object.assign(dto, result);
+    return dto;
+  }
+}
+
+export class RefreshResponseDto {
+  accessToken!: string;
+
+  static fromResult(result: RefreshResult): RefreshResponseDto {
+    const dto = new RefreshResponseDto();
     Object.assign(dto, result);
     return dto;
   }
